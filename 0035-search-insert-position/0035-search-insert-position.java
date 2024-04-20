aa934c2401a11790220992a869bff5fa27f9b290
hanhyeyeon-dev <dusdl7070@gmail.com>
2024-04-21 08:55:47 +0900

    Time: 0 ms (100.00%), Space: 43.2 MB (16.13%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1; // 3
+        
+        while(start <= end) {
+            int mid = start + (end - start)/2; // 1
+            if (nums[mid] == target) { // 3 != 5, false
+                return mid;
+            } else if (nums[mid] < target) { // 3 < 5, true
+                start = mid + 1; // 1
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+}
